04184b305f023d7e76f29634877fd90092893d8b
duvan <duvanlv20@hotmail.com>
2025-12-16 20:32:51 +0100

    fvbgf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("com.google.zxing:core:3.5.1")
+
+
+
 }
